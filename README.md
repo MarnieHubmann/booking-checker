09d48ea10354012f2147eec5b0ad17aae3d93835
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2023-01-10 12:38:47 +1300

    Split automation into functions and use gmail to send notifications

--- README.md
+++ README.md
@@ -2,13 +2,31 @@
 
 Checks Waka Kotahi NZTA practical drivers licence bookings and rebooks them to an earlier date if possible.
 
+## Requirements
+
+Node 16 and Yarn are required
+
 ## Local Setup
 
 Make a copy of `.env.dist` and name it `.env`
 In `.env`, set the values to be the details of the driver you wish to make bookings on behalf of.
 
+To install depedencies:
+
+```sh
+yarn && npx playwright install --with-deps
+```
+
 To run it locally:
 
 ```sh
 npx playwright test drivers-test.spec.ts
+```
+
+## Scheduling
+
+You can clone this repo on a server and follow the setup instructions, and then add the following entry to your crontab:
+
+```sh
+
 ```
--- drivers-test.spec.ts
+++ drivers-test.spec.ts
@@ -1,23 +1,170 @@
-import { test } from "@playwright/test";
-import * as dotenv from "dotenv";
+import { Page, test } from "@playwright/test";
 import { isBefore, isToday, parse } from "date-fns";
+import { readdirSync, rmSync } from "fs";
+import nodemailer, { SentMessageInfo } from "nodemailer";
+import smtpTransport from "nodemailer-smtp-transport";
+import { Attachment } from "nodemailer/lib/mailer";
+
+function getLicenceDetails() {
+  if (!process.env?.LICENCE_NUMBER) {
+    throw new Error(
+      "Please set the LICENCE_NUMBER environment variable or value in .env"
+    );
+  }
+  if (!process.env?.LICENCE_VERSION) {
+    throw new Error(
+      "Please set the LICENCE_VERSION environment variable or value in .env"
+    );
+  }
+  if (!process.env?.LAST_NAME) {
+    throw new Error(
+      "Please set the LAST_NAME environment variable or value in .env"
+    );
+  }
+  if (!process.env?.DATE_OF_BIRTH) {
+    throw new Error(
+      "Please set the DATE_OF_BIRTH environment variable or value in .env"
+    );
+  }
+  return {
+    licenceNumber: process.env.LICENCE_NUMBER,
+    licenceVersion: process.env.LICENCE_VERSION,
+    lastName: process.env.LAST_NAME,
+    dateOfBirth: process.env.DATE_OF_BIRTH,
+  };
+}
+
+function setupEmailer() {
+  // Check gmail credentials are set
+  if (!process.env?.GMAIL_TO) {
+    throw new Error(
+      "Please set the GMAIL_TO environment variable or value in .env"
+    );
+  }
+  if (!process.env?.GMAIL_FROM) {
+    throw new Error(
+      "Please set the GMAIL_FROM environment variable or value in .env"
+    );
+  }
+  if (!process.env?.GMAIL_PASSWORD) {
+    throw new Error(
+      "Please set the GMAIL_PASSWORD environment variable or value in .env"
+    );
+  }
+
+  const transporter = nodemailer.createTransport(
+    smtpTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      auth: {
+        user: process.env.GMAIL_FROM,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    })
+  );
+
+  return async ({
+    subject,
+    text,
+    attachments,
+  }: {
+    subject: string;
+    text: string;
+    attachments: Attachment[];
+  }): Promise<SentMessageInfo> =>
+    transporter.sendMail({
+      from: process.env.GMAIL_FROM,
+      to: process.env.GMAIL_TO,
+      subject,
+      text,
+      attachments,
+    });
+}
+
+async function checkIfSlotIsAppropriate(
+  page: Page,
+  currentBookingDate: Date
+): Promise<boolean> {
+  let newDateText = await page.locator(".dateText").first().innerText();
+  if (!newDateText) {
+    console.log("Couldn't find date on this page");
+  }
+  let newDate = parse(newDateText, "EEEE d MMMM y", new Date());
+  if (isToday(newDate)) {
+    console.log("Found a booking that is today, but that is too soon", newDate);
+    return false;
+  }
+  if (!isBefore(newDate, currentBookingDate)) {
+    console.log("Selected booking date is after the current booking", newDate);
+    return false;
+  }
+  if (!isBefore(newDate, new Date())) {
+    console.log("Selected booking date is too soon/in the past", newDate);
+    return false;
+  }
+  return true;
+}
+
+async function selectNextBooking(page: Page, currentBookingDate: Date) {
+  let screenshotsTaken = 0;
+
+  // Look at the first available booking on the first page
+  let result = await checkIfSlotIsAppropriate(page, currentBookingDate);
+  await page.screenshot({
+    path: `${__dirname}/screenshots/bookings-page-${++screenshotsTaken}.png`,
+    fullPage: true,
+  });
+  if (result) {
+    return true;
+  }
+
+  // Look at the next available booking on the first page
+  if (
+    (
+      await page
+        .locator("td:not(.ui-state-disabled):not(.ui-datepicker-current-day)")
+        .all()
+    ).length
+  ) {
+    await page
+      .locator("td:not(.ui-state-disabled):not(.ui-datepicker-current-day)")
+      .first()
+      .click();
+    await page.waitForTimeout(3000);
+    let result = await checkIfSlotIsAppropriate(page, currentBookingDate);
+    await page.screenshot({
+      path: `${__dirname}/screenshots/bookings-page-${++screenshotsTaken}.png`,
+      fullPage: true,
+    });
+    if (result) {
+      return true;
+    }
+  }
+
+  // Go to the next month and see if anything is available
+  await page.click("a.ui-datepicker-next");
+  await page.waitForTimeout(3000);
+  result = await checkIfSlotIsAppropriate(page, currentBookingDate);
+  await page.screenshot({
+    path: `${__dirname}/screenshots/bookings-page-${++screenshotsTaken}.png`,
+    fullPage: true,
+  });
+  return result;
+}
+
+function getScreenshotsAsAttachments(): Attachment[] {
+  return readdirSync(__dirname + "/screenshots").map((file) => ({
+    path: __dirname + "/screenshots/" + file,
+    filename: file,
+  }));
+}
 
 test("check drivers licence test slots and change it if there is an earlier spot available", async ({
   page,
 }) => {
-  dotenv.config();
-  if (!process.env?.LICENCE_NUMBER) {
-    throw new Error("Please set the LICENCE_NUMBER environment variable or value in .env");
-  }
-  if (!process.env?.LICENCE_VERSION) {
-    throw new Error("Please set the LICENCE_VERSION environment variable or value in .env");
-  }
-  if (!process.env?.LAST_NAME) {
-    throw new Error("Please set the LAST_NAME environment variable or value in .env");
-  }
-  if (!process.env?.DATE_OF_BIRTH) {
-    throw new Error("Please set the DATE_OF_BIRTH environment variable or value in .env");
-  }
+  rmSync(__dirname + "/screenshots", { recursive: true, force: true });
+  const licenceDetails = getLicenceDetails();
+  const sendEmail = setupEmailer();
 
   // Visit NZTA test bookings portal
   await page.goto("https://online.nzta.govt.nz/licence-test/");
@@ -26,16 +173,16 @@ test("check drivers licence test slots and change it if there is an earlier spot
   // Fill in drivers licence details and login
   await page.type(
     'input[formcontrolname="LicenceNumber"]',
-    process.env.LICENCE_NUMBER
+    licenceDetails.licenceNumber
   );
   await page.type(
     'input[formcontrolname="LicenceVersion"]',
-    process.env.LICENCE_VERSION
+    licenceDetails.licenceVersion
   );
-  await page.type('input[formcontrolname="LastName"]', process.env.LAST_NAME);
+  await page.type('input[formcontrolname="LastName"]', licenceDetails.lastName);
   await page.type(
     'input[formcontrolname="DateOfBirth"]',
-    process.env.DATE_OF_BIRTH
+    licenceDetails.dateOfBirth
   );
   await page.click('button:text("Login")');
 
@@ -49,6 +196,10 @@ test("check drivers licence test slots and change it if there is an earlier spot
     "EEEE d MMMM y, h:m aa",
     new Date()
   );
+  await page.screenshot({
+    path: __dirname + "/screenshots/current-booking.png",
+    fullPage: true,
+  });
   await page.click('button:text("Reschedule")');
 
   // Select the Thordon test site
@@ -58,19 +209,9 @@ test("check drivers licence test slots and change it if there is an earlier spot
   await page.waitForTimeout(3000);
 
   // Find the next soonest available booking
-  let newDateText = await page.locator(".dateText").first().innerText();
-  while (!newDateText.length) {
-    await page.click("a.ui-datepicker-next");
-    await page.waitForTimeout(3000);
-    newDateText = await page.locator(".dateText").first().innerText();
-  }
-  const newDate = parse(newDateText, "EEEE d MMMM y", new Date());
+  const foundNewBooking = await selectNextBooking(page, currentBookingDate);
 
-  if (
-    isBefore(newDate, currentBookingDate) &&
-    !isToday(newDate) &&
-    !isBefore(newDate, new Date())
-  ) {
+  if (foundNewBooking) {
     // There is a new booking time - rebook it
     await page.locator("input").first().click();
     await page.waitForTimeout(3000);
@@ -80,13 +221,24 @@ test("check drivers licence test slots and change it if there is an earlier spot
     await page.waitForTimeout(5000);
     await page.click('button:text("Continue")');
 
-    await page.screenshot({ path: "screenshots/changed-booking.png", fullPage: true });
-    console.log("Successfully rebooked test for " + newDateText);
+    await page.screenshot({
+      path: __dirname + "/screenshots/new-booking.png",
+      fullPage: true,
+    });
+    await sendEmail({
+      subject: "Drivers Licence Booking: REBOOKED!",
+      text: "Successfully rebooked test!",
+      attachments: getScreenshotsAsAttachments(),
+    });
+    console.log("Successfully rebooked test!");
   } else {
     // Nothing sooner is available so just log out
-    await page.screenshot({ path: "screenshots/no-bookings-available.png", fullPage: true });
     await page.click('a:text("(Log out)")');
+    await sendEmail({
+      subject: "Drivers Licence Booking: No bookings available",
+      text: "No sooner bookings were available :(",
+      attachments: getScreenshotsAsAttachments(),
+    });
     console.log("No sooner bookings were available :(");
-    test.fail(true, "No sooner bookings were available :(");
   }
 });
